d6d60d36346984a54c0db8ffdd69074c4eb12be4
unknown <chenhaorui2002@gmail.com>
2024-11-13 17:49:46 -0500

    Final Version

--- Lab5/DMem/DMem.v
+++ Lab5/DMem/DMem.v
@@ -1,32 +1,33 @@
-module Data_Memory(Address, WriteData, MemoryRead, MemoryWrite, ReadData, clk, rst);
+module Data_Memory(
+    input [$clog2(MEMSIZE)-1:0] Address,
+    input [BITSIZE-1:0] WriteData,
+    input MemoryRead, MemoryWrite,
+    output reg [BITSIZE-1:0] ReadData,
+    input clk, rst
+);
 
     parameter BITSIZE = 32;
     parameter MEMSIZE = 64;
-    input [$clog2(MEMSIZE)-1:0] Address;
-    input [BITSIZE-1:0] WriteData;
-    input MemoryRead, MemoryWrite;
-    output reg [BITSIZE-1:0] ReadData;
-    input clk, rst;
-
-    reg [BITSIZE-1:0] mem_file [MEMSIZE-1:0];   // Entire list of registers
-
-    integer i;                                  // Used below to rst all registers
     
-    always @(Address, mem_file[Address])
-    begin
-        if (MemoryRead)
-            ReadData <= mem_file[Address];
+    reg [BITSIZE-1:0] mem_file [MEMSIZE-1:0];
+    integer i;
+    
+    always @(*) begin
+        if (MemoryWrite) begin
+            mem_file[Address] = WriteData; 
+        end
     end
 
-    always @(posedge clk or posedge rst)
-        begin
-            if (rst)
-                for (i=0; i<MEMSIZE; i=i+1) mem_file[i] <= i; // rst all registers
-            else
-            begin        
-                if (MemoryWrite)
-                    mem_file[Address] <= WriteData; //If writeback is enabled and not register0.
-            end
+    always @(*) begin
+        if (MemoryRead)
+            ReadData = mem_file[Address]; 
+    end
+
+    always @(posedge rst) begin
+        if (rst) begin
+            for (i = 0; i < MEMSIZE; i = i + 1)
+                mem_file[i] <= i; 
         end
+    end
 
 endmodule
